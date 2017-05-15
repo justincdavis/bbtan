4ca9a4f15f310d99fff3905b654e62dd1821118e
Justin Davis <davis.justin@mssm.org>
2017-05-15 11:01:17 -0400

    grid

--- grid.h
+++ grid.h
@@ -1,11 +1,33 @@
 #ifndef GRID_H
 #define GRID_H
 
+#include "graphics.h"
+#include "vec2d.h"
+#include <vector>
+
+using namespace mssm;
+using namespace std;
 
 class Grid
 {
 public:
+    int rows;
+    int cols;
+    int margin_left;
+    int margin_right;
+    int margin_top;
+    int margin_bottom;
+    int colWidth;
+    int rowHeight;
+public:
     Grid();
+    void draw(Graphics& g);
+    void fillCell(Graphics& g, int row, int col, Color c, double fillAmount);
+    int cellLeftPos(Graphics& g, int col);
+    int cellTopPos(Graphics& g, int row);
+    int getRow(Graphics& g, int mouseY);
+    int getCol(Graphics& g, int mouseX);
+    bool validRowAndCol(int row, int col);
 };
 
 #endif // GRID_H
